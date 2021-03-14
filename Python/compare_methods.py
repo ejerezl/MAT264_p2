20b0208fd09720025be82291878713cdffa0f38b
Esther Jerez <estherjerez99@gmail.com>
2021-03-14 10:54:35 +0100

    graph values

--- Python/compare_methods.py
+++ Python/compare_methods.py
@@ -9,11 +9,11 @@ import os
 cwd = os.getcwd()
 plt.style.use(cwd + '/poster.mplstyle')
 
-save = False
+save = True
 '~~~~~~~~~~ SELECT THE PROBLEM FUNCTIONS ~~~~~~~~~~'
-name_of_problem = 'traffic'
+#name_of_problem = 'traffic'
 #name_of_problem = 'linear'
-#name_of_problem = 'Burger'
+name_of_problem = 'Burger'
 #name_of_problem = 'Buckley-Leverett'
 
 
@@ -30,16 +30,16 @@ name_of_init = 'police problem'
 
 c = 1
 
-T = 15
-X = 20
-h = 0.1
+T = 5
+X = 10
+h = 0.01
 
 X, h, x_step, T, k, t_step, U_0 = init.get_initial(name_of_init, U=0, X=X, T=T, h=h)
 
 f, f_prime, speed = init.get_problemfunction(name_of_problem, c=c)
 
 sols = []
-methods = ['Godunov', 'Lax-Wendsdroff from lecture', 'Lax-Friedrichs']
+methods = ['Lax-Wendsdroff from lecture', 'Godunov', 'Lax-Friedrichs']
 '------------------ SOLVE THE PDE WITH ALL METHODS -----------------------------------------------------------------------------------------'
 for method in methods:
     sols.append(step.solve_problem(method, U_0, X, T, h, k, x_step, t_step, f, f_prime))
@@ -58,23 +58,28 @@ elif col_span == 1:
     matplotlib.rcParams['figure.figsize'] = (6.8025, 5.101875001*1.25)
 
 i = 0
-colors = ['darkorange', 'navy', 'green']
-name = ['Godunov', 'Lax-Wendroff', 'Lax-Friedrichs']
+colors = ['navy', 'darkorange', 'green']
+name = ['Lax-Wendroff', 'Godunov', 'Lax-Friedrichs']
 x_steplist = np.arange(0, X, h)
 for method in name:
     plt.plot(x_steplist, sols[i][-1], label='\\rmfamily ' + method, linewidth=lw, color = colors[i]) #Plotting the solution for each method
     i += 1
 
-plt.xlim([10, 20])
+if name_of_problem == 'linear':
+    real_sol = step.get_true_linear_sol(U_0, t_step, h, k, c)[0]
+    plt.plot(x_steplist, real_sol, ':', label='\\rmfamily ' + 'Real solution', linewidth=lw, color = 'dimgrey')
+    plt.xlim([4.4, 7.5])
+elif name_of_problem == 'Burgers':
+    plt.xlim([1, 5])
 
 plt.title('\\rmfamily\\bfseries Solutions', pad=15)
 plt.xlabel('\\rmfamily Space')
-plt.gca().xaxis.set_label_coords(0.5, -0.25)
+plt.gca().xaxis.set_label_coords(0.5, -0.175)
 plt.ylabel('\\rmfamily Density')
-plt.legend(bbox_to_anchor=(0., -0.5, 1., .102), loc='upper left',
+plt.legend(bbox_to_anchor=(0., -0.425, 1., .102), loc='upper left',
             ncol=2, mode="expand", borderaxespad=0., handlelength=1., fontsize=21.6)
 
 if save:
-    plt.savefig('figures/compare_methods.png', dpi=300)
+    plt.savefig('figures/linear_comparing.png', dpi=300)
 plt.show()
 
